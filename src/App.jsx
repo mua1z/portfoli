a70587783fb6824cd43c23cdf5329a68b37003be
Muaz <muazddu@gmail.com>
2025-05-04 23:53:43 +0300

    structure and formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
+import './index.css'
+
+
+
+
 const App = () => {
   return (
+    <div>
     <h1 class="text-3xl font-bold underline bg-amber-500">
-    Hello world!
-  </h1>
+    Hello world! </h1>
+
+    <h2 class="text-2xl font-bold underline bg-amber-500">
+    Muaz Amin </h2>
+    </div>
   )
 }
 
