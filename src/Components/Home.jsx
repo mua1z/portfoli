808045b887b2a2752e012583bf97971189988fbb
Muaz <muazddu@gmail.com>
2025-05-05 18:50:05 +0300

    2nd commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Photo from '../assets/Photo/photo_2025-04-21_14-01-08.jpg'
+
 
 const Home = () => {
   return (
@@ -8,7 +10,10 @@ const Home = () => {
             <h1 className='text-2xl md:text-5xl font-bold flex leading-normal tracking-tighter'>Hey There! I'm Obsa Amin</h1>
             <h6 className='text-2xl md:text-4xl font-bold flex leading-normal tracking-tighter'>Banking and Financial Professional</h6>
             <p className='text-sm md:text-2xl tracking-tight'>With a strong passion for delivering secure, efficient, and customer-focused financial solutions, I bring proven experience in banking operations, loan processing, and financial compliance. Whether supporting digital banking platforms or working directly with clients, I strive to ensure trust, accuracy, and excellence in every financial transaction.</p>
+
+            <button className='mt-5 md:md-10 text-white py02 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-blue-300 '>Contact Me</button>
             </div>
+            <div><img className='w-2/5 rounded-full' src={Photo} alt='Obsa'></img></div>
     </div>
   )
 }
