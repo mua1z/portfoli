110e5bbe41eb5e6689870aa65cc1e03ab69f0fb6
Muaz <muazddu@gmail.com>
2025-05-05 17:13:03 +0300

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
 
 import './index.css'
 
 
 
 
-const App = () => {
+function App() {
   return (
-    <div>
-    <h1 class="text-3xl font-bold underline bg-amber-500">
-    Hello world! </h1>
-
-    <h2 class="text-2xl font-bold underline bg-amber-500">
-    Muaz Amin </h2>
+    <div className='bg-[#171d32] h-auto w-full overflow-hidden'>
+    <Navbar/>
     </div>
   )
 }
